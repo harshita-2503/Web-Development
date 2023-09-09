9b1348e02bc4c8287fa8de0b5aef955c331b9f3a
harshita-2503 <112571316+harshita-2503@users.noreply.github.com>
2023-09-09 20:15:06 +0530

    Add files via upload

---
+++ Js-4/Method.js
@@ -0,0 +1,29 @@
+const calculator={
+    num: 55,
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b)
+    {
+        return a*b;
+    }
+};
+
+
+//Shorthand Method for method
+const cal={
+    add(a,b){
+        return a+b;
+    },
+    sub(a,b){
+        return a-b;
+    },
+    mul(a,b)
+    {
+        return a*b;
+    }
+}
+
