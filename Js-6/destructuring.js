bb0aa32b1397eaa07afa4d181f71d6715f2b4018
harshita-2503 <112571316+harshita-2503@users.noreply.github.com>
2023-09-15 21:08:45 +0530

    Delete Js-6 directory

--- Js-6/destructuring.js
+++
@@ -1,6 +0,0 @@
-let names=["tony","bruce","peter","steve","abs","xyz"];
-// let winner=names[0];
-// let runnerup=names[1];
-// let secondRunnerup=names[2];
-let [winner,runnerup, secondRunnerup]=names;
-let [first,sec, ...others]=names;
